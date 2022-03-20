8c39967d649cd9041a818798574207844c9e6121
Nikol <39650438+nikolst@users.noreply.github.com>
2022-03-20 21:02:25 +0200

    version_2
    Save the account information in Database.

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ package bank;
 public class Main {
 
     public static void main(String[] args) {
+
+        AccountsDatabase ad = new AccountsDatabase();
+        ad.connect();
+
         Menu menu = new Menu();
         menu.accountMenuSwitch();
     }
